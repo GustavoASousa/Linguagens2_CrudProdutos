3e086703741edc9800771e4198d997854f0e8710
GustavoASousa <55192831+GustavoASousa@users.noreply.github.com>
2020-08-27 19:21:55 -0300

    Add files via upload

---
+++ crud_produtos/ProdutoDao.java
@@ -0,0 +1,15 @@
+package crud_produtos;
+
+import java.util.LinkedList;
+
+
+public class ProdutoDao {
+private static LinkedList<Produto> lista = new LinkedList<Produto>();
+	
+	public void inserir(Produto p) {
+		lista.add(p);
+	}
+	public LinkedList<Produto> listar() {
+		return lista;
+	}
+}
